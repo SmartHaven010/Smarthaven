1edee67b63817b3c6fc141bba261b20dcc140d79
SmartHaven010 <cleancleans010@outlook.com>
2025-09-02 23:10:29 +0200

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,13 @@
+import Hero from "../components/Hero";
+import Feature from "../components/Feature";
+import Footer from "../components/Footer";
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <Feature />
+      <Footer />
+    </main>
+  );
+}
